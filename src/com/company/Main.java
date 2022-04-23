3ac077b95924710c87ca1a3e40c20795c28faa0f
artem <de_artem@inbox.ru>
2022-04-23 18:16:08 +1100

    Выполненное домашнее задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,14 @@ package com.company;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+        Author author1 = new Author("Ekaterina", "Andreeva");
+        Book book1 = new Book("Kolobok", author1,1950);
+        book1.setPublicationAge(1972);
+        Author author2 = new Author("Napoleon", "Hill");
+        Book book2 = new Book("Think and grow rich", author2, 2000);
+
     }
 }
